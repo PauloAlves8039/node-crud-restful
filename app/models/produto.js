9ad576a6cf8602972369381c8e35ed138860cb70
PauloAlves8039 <pj38alves@gmail.com>
2020-04-16 22:30:50 -0300

    Commit inicial do repositório

---
+++ app/models/produto.js
@@ -0,0 +1,9 @@
+/**
+ * @file: produto.js
+ * @author: Paulo Alves
+ * @description: responsável pela representação da entidade 'Produto'.
+ * @version 1.0.1(16/04/2020)
+ */
+
+
+
---
+++ server.js
@@ -0,0 +1,7 @@
+/**
+ * @file: server.js
+ * @author: Paulo Alves
+ * @description: responsável pelas configurações do servidor.
+ * @version 1.0.1(16/04/2020)
+ */
+
